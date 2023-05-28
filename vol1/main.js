b6e9fb2f7c5ef13fe515b9f6080d84bc842eeffd
lilghost17 <133694872+lilghost17@users.noreply.github.com>
2023-05-28 12:34:45 +0400

    2 დავალება

--- vol1/main.js
+++ vol1/main.js
@@ -1,29 +1,4 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+let input1=document.getElementById('input1');
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
+input1.addEventListener('keydown',()=>{
+    document.querySelector('section').style.backgroundColor=input1.value;})
